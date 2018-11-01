4a2011ae6f3da34711bd7c30353a3588cfa50758
Nilaksh Das <nilakshdas@gatech.edu>
2018-11-01 11:52:34 -0400

    refactored training

--- goggles/train.py
+++ goggles/train.py
@@ -7,10 +7,9 @@ import os
 import numpy as np
 from PIL import Image
 import torch
-import torch.nn as nn
+from torch.utils.data import DataLoader
 import torch.optim as optim
 import torchvision.transforms as transforms
-import torchvision.utils as vutils
 from tqdm import tqdm
 
 from constants import *
@@ -19,13 +18,17 @@ from loss import my_loss_function
 from models.semantic_ae import SemanticAutoencoder
 
 
+is_cuda = torch.cuda.is_available()
+put_on_gpu = lambda x: x.cuda() if is_cuda else x
+
+
 def reproject(model, dataset):
     num_prototypes = model.num_prototypes
 
     all_patches = list()
     for image, label, attributes, _ in dataset:
         x = image.view((1,) + image.size())
-        x = torch.autograd.Variable(x)
+        x = put_on_gpu(torch.autograd.Variable(x))
         z, z_patches, reconstructed_x = model(x)
 
         patches = z_patches[0]
@@ -44,83 +47,111 @@ def reproject(model, dataset):
         model.prototypes.weight[j].data = torch.FloatTensor(nearest_patch)
 
 
+def load_datasets(input_image_size, *dataset_args):
+    assert len(dataset_args) == 3
+
+    transform_random_flip = transforms.RandomHorizontalFlip()
+    transform_scale =  transforms.Scale((input_image_size, input_image_size))
+    transform_to_tensor = transforms.ToTensor()
+    transform_normalize = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+
+    random_transformation = transforms.Compose([
+        transform_random_flip, transform_scale, transform_to_tensor, transform_normalize])
+    deterministic_transformation = transforms.Compose([
+        transform_scale, transform_to_tensor, transform_normalize])
+
+    train_dataset_random = CUBDataset(
+        *dataset_args, transform=random_transformation, is_training=True)
+
+    train_dataset_deterministic = CUBDataset(
+        *dataset_args, transform=deterministic_transformation, is_training=True)
+
+    test_dataset = CUBDataset(
+        *dataset_args, transform=deterministic_transformation, is_training=False)
+
+    return train_dataset_random, train_dataset_deterministic, test_dataset
+
+
+def cub_dataset_batch_collate(batch):
+    batch = zip(*batch)  # transpose
+
+    image, label, attributes, \
+    num_nonzero_attributes = batch
+
+    image = torch.stack(image)
+    label = torch.LongTensor(label)
+    attributes = torch.stack([torch.LongTensor(a) for a in attributes])
+    padding_idx = torch.LongTensor(num_nonzero_attributes)
+
+    return image, label, attributes, padding_idx
+
+
+def save_tensor_as_image(x, filepath):
+    image = x.data.cpu().numpy()[0]
+    image = 255. * (0.5 + (image / 2.))
+    image = np.array(image, dtype=np.uint8)
+    image = np.swapaxes(image, 0, 1)
+    image = np.swapaxes(image, 1, 2)
+    image = Image.fromarray(image)
+    image.save(filepath)
+
+
 def main():
+    input_image_size = 64
     species1_id = 14
     species2_id = 90
-
-    input_image_size = 64
     patch_size = 1
+    num_epochs = 100
 
-    train_dataset_random = CUBDataset(root=CUB_DATA_DIR,
-                                      species1_id=species1_id, species2_id=species2_id,
-                                      transform=transforms.Compose([
-                                          transforms.RandomHorizontalFlip(),
-                                          transforms.Scale((input_image_size, input_image_size)),
-                                          transforms.ToTensor(),
-                                          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
-                                      is_training=True)
+    train_dataset_random, train_dataset_deterministic, \
+        test_dataset = load_datasets(input_image_size, CUB_DATA_DIR, species1_id, species2_id)
 
-    train_dataset_deterministic = CUBDataset(root=CUB_DATA_DIR,
-                                             species1_id=species1_id, species2_id=species2_id,
-                                             transform=transforms.Compose([
-                                                 transforms.Scale((input_image_size, input_image_size)),
-                                                 transforms.ToTensor(),
-                                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
-                                             is_training=True)
+    train_dataloader = DataLoader(
+        train_dataset_random,
+        collate_fn=cub_dataset_batch_collate,
+        batch_size=4, shuffle=True)
 
-    test_dataset = CUBDataset(root=CUB_DATA_DIR,
-                              species1_id=species1_id, species2_id=species2_id,
-                              transform=transforms.Compose([
-                                  transforms.Scale((input_image_size, input_image_size)),
-                                  transforms.ToTensor(),
-                                  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
-                              is_training=False)
+    model = put_on_gpu(SemanticAutoencoder(
+        input_image_size,
+        patch_size,
+        train_dataset_random.num_attributes))
 
-    train_shuffled_dataloader = torch.utils.data.DataLoader(train_dataset_random, batch_size=4, shuffle=True)
-
-    model = SemanticAutoencoder(input_image_size, patch_size, train_dataset_random.num_attributes)
     optimizer = optim.Adam(ifilter(lambda p: p.requires_grad, model.parameters()))
 
-    num_epochs = 100
     pbar = tqdm(range(1, num_epochs + 1))
     for epoch in pbar:
-        total_loss = 0.
-        for i, batch in enumerate(train_shuffled_dataloader, 1):
+        epoch_loss = 0.
+
+        for i, (image, label, attributes, padding_idx) in enumerate(train_dataloader, 1):
             model.zero_grad()
 
-            x = torch.autograd.Variable(batch[0])
+            x = put_on_gpu(torch.autograd.Variable(image))
             z, z_patches, reconstructed_x = model(x)
 
-            attributes = torch.stack(batch[2]).t()  # batch_size, num_attributes
-            attribute_prototypes = model.prototypes(attributes)  # batch_size, num_attributes, embedding_dim
-            padding_idx = batch[3]
+            attributes = put_on_gpu(attributes)
+            attribute_prototypes = model.prototypes(attributes)
 
             loss = my_loss_function(reconstructed_x, z_patches, attribute_prototypes, padding_idx, x)
 
             loss.backward()
             optimizer.step()
 
-            total_loss += loss.data[0]
+            epoch_loss += loss.data[0]
 
-        pbar.set_description('average_epoch_loss=%0.05f' % (total_loss / i))
+        pbar.set_description('average_epoch_loss=%0.05f' % (epoch_loss / i))
 
-        if (epoch % 5 == 0) or (epoch == 1):  # TODO: reset
+        if (epoch % 5 == 0) or (epoch == num_epochs):
             reproject(model, train_dataset_deterministic)
 
         for i, (image, label, attributes, _) in enumerate(test_dataset):
             x = image.view((1,) + image.size())
-            x = torch.autograd.Variable(x)
+            x = put_on_gpu(torch.autograd.Variable(x))
             z, z_patches, reconstructed_x = model(x)
 
-            reconstructed_image = reconstructed_x.data.cpu().numpy()[0]
-            reconstructed_image = 255. * (0.5 + (reconstructed_image / 2.))
-            reconstructed_image = np.array(reconstructed_image, dtype=np.uint8)
-            reconstructed_image = np.swapaxes(reconstructed_image, 0, 1)
-            reconstructed_image = np.swapaxes(reconstructed_image, 1, 2)
-            reconstructed_image = Image.fromarray(reconstructed_image)
-            reconstructed_image.save(os.path.join(OUT_DIR, 'images', '%d-%d.png' % (epoch, i)))
+            save_tensor_as_image(
+                reconstructed_x, os.path.join(OUT_DIR, 'images', '%d-%d.png' % (epoch, i)))
 
-    model.save_state_dict(os.path.join(MODEL_DIR, 'model.pt'))
+    torch.save(model, os.path.join(MODEL_DIR, 'model.pt'))
 
 
 if __name__ == '__main__':
