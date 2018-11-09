ba78a47332e2e981308cc42fc7ab0f7aacd0c571
Nilaksh Das <nilakshdas@gatech.edu>
2018-11-09 13:45:26 -0500

    refactored model functionality

--- goggles/models/semantic_ae.py
+++ goggles/models/semantic_ae.py
@@ -20,20 +20,20 @@ class SemanticAutoencoder(nn.Module):
         self._encoder_net = Encoder(input_size)
         self._decoder_net = Decoder(self._encoder_net.num_out_channels)
 
-
         encoded_output_size = self._encoder_net.output_size
-        assert encoded_patch_size < encoded_output_size
+        assert encoded_patch_size <= encoded_output_size
         self._patches = Patch.from_spec(
             (encoded_output_size, encoded_output_size),
             (encoded_patch_size, encoded_patch_size))
+
+        self._receptive_fields_for_patches = dict()
 
         dim_prototypes = self._encoder_net.num_out_channels * (encoded_patch_size ** 2)
         self.prototypes = Prototypes(num_prototypes + 1, dim_prototypes, padding_idx=0)
         self.prototypes.weight.requires_grad = False  # freeze embeddings
 
-    def cuda(self, device_id=None):
-        self._is_cuda = True
-        return super(SemanticAutoencoder, self).cuda(device_id)
+    def _make_cuda(self, x):
+        return x.cuda() if self._is_cuda else x
 
     def forward(self, x):
         z = self._encoder_net(x)
@@ -45,14 +45,46 @@ class SemanticAutoencoder(nn.Module):
 
         return z, z_patches, reconstructed_x
 
-    def get_nearest_dataset_patches_for_prototypes(self, dataset):
-        put_on_gpu = lambda x_: x_.cuda() if self._is_cuda else x_
+    def cuda(self, device_id=None):
+        self._is_cuda = True
+        return super(SemanticAutoencoder, self).cuda(device_id)
 
+    def get_receptive_field_for_patch(self, patch_idx):
+        if patch_idx not in self._receptive_fields_for_patches:
+            self.zero_grad()
+
+            image_size = self.input_size
+            batch_shape = (1, 3, image_size, image_size)
+
+            x = self._make_cuda(torch.autograd.Variable(
+                torch.rand(*batch_shape), requires_grad=True))
+            z = self._encoder_net.forward(x)
+            z_patch = self._patches[patch_idx].forward(z)
+
+            torch.sum(z_patch).backward()
+
+            rf = x.grad.data.cpu().numpy()
+            rf = rf[0, 0]
+            rf = zip(*np.where(np.abs(rf) > 1e-6))
+
+            (i_nw, j_nw), (i_se, j_se) = rf[0], rf[-1]
+
+            rf_w, rf_h = (j_se - j_nw + 1,
+                          i_se - i_nw + 1)
+
+            self._receptive_fields_for_patches[patch_idx] = \
+                (i_nw, j_nw), (rf_w, rf_h)
+
+            self.zero_grad()
+
+        return self._receptive_fields_for_patches[patch_idx]
+
+    def get_nearest_patches_for_prototypes(self, dataset):
         all_patches = list()
         all_patches_indices = list()
         for i, (image, _, _, _) in enumerate(dataset):
             x = image.view((1,) + image.size())
-            x = put_on_gpu(torch.autograd.Variable(x))
+            x = self._make_cuda(torch.autograd.Variable(x))
             z, z_patches, reconstructed_x = self.forward(x)
 
             patches = z_patches[0]
@@ -61,52 +93,23 @@ class SemanticAutoencoder(nn.Module):
                 all_patches_indices.append((i, j))
         all_patches = np.array(all_patches)
 
-        prototype_patches = dict()
+        nearest_patches_for_prototypes = dict()
         for k in range(1, self.num_prototypes + 1):
             prototype = self.prototypes.weight[k].data.cpu().numpy()
 
             dists = np.linalg.norm(prototype - all_patches, ord=2, axis=1)
 
             nearest_patch_idx = np.argmin(dists)
-            nearest_patch = all_patches[nearest_patch_idx]
+            nearest_patch = torch.FloatTensor(all_patches[nearest_patch_idx])
             nearest_image_patch_idx = all_patches_indices[nearest_patch_idx]  # (image_idx, patch_idx)
-            prototype_patches[k] = (nearest_image_patch_idx, nearest_patch)
+            nearest_patches_for_prototypes[k] = (nearest_image_patch_idx, nearest_patch)
 
-        return prototype_patches
+        return nearest_patches_for_prototypes
 
-    def reproject_prototypes(self, prototype_patches):
-        for k, (nearest_image_patch_idx, nearest_patch) in prototype_patches.items():
-            self.prototypes.weight[k].data = torch.FloatTensor(nearest_patch)
-
-        return prototype_patches
-
-    def get_receptive_field(self, patch_idx):
-        put_on_gpu = lambda x_: x_.cuda() if self._is_cuda else x_
-
-        self.zero_grad()
-
-        image_size = self.input_size
-        batch_shape = (1, 3, image_size, image_size)
-
-        x = put_on_gpu(torch.autograd.Variable(
-            torch.rand(*batch_shape), requires_grad=True))
-        z = self._encoder_net.forward(x)
-        z_patch = self._patches[patch_idx].forward(z)
-
-        torch.sum(z_patch).backward()
-
-        rf = x.grad.data.cpu().numpy()
-        rf = rf[0, 0]
-        rf = zip(*np.where(np.abs(rf) > 1e-6))
-
-        (i_nw, j_nw), (i_se, j_se) = rf[0], rf[-1]
-
-        rf_w, rf_h = (j_se - j_nw + 1,
-                      i_se - i_nw + 1)
-
-        self.zero_grad()
-
-        return (i_nw, j_nw), (rf_w, rf_h)
+    def reproject_prototypes(self, nearest_patches_for_prototypes):
+        for k, (nearest_image_patch_idx, nearest_patch) \
+                in nearest_patches_for_prototypes.items():
+            self.prototypes.weight[k].data = nearest_patch
 
 
 if __name__ == '__main__':
@@ -128,4 +131,4 @@ if __name__ == '__main__':
 
     # print net.prototypes.weight[1]
 
-    print net.get_receptive_field(0)
+    print net.get_receptive_field_for_patch(0)
