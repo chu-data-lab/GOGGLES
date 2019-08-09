eea83218813fbbe021dcb6319705280f32ad7b73
wurenzhi <wurenzhi@foxmail.com>
2019-08-09 14:03:12 -0400

    feasibility test, dev set sufficiency test

--- goggles/theory/theory.py
+++ goggles/theory/theory.py
@@ -1,6 +1,7 @@
 from scipy.stats import beta
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 
 def generate_d_matrix(n, acc, K):
@@ -19,12 +20,35 @@ def generate_d_matrix(n, acc, K):
         row_i[i] = int(n * acc)
         d_rows.append(row_i)
     d = np.squeeze(np.array(d_rows))
+    print(np.trace(d)/np.sum(d))
     return d
 
+def n_given_sum(n_class,n_sum,n_max,x_0):
+    n_given_sum_list = [[None for _ in range(n_sum+1)] for _ in range(n_class+1)]
+    def _n_given_sum_(n_class,n_sum,n_max):
+        if n_given_sum_list[n_class][n_sum] is not None:
+            return n_given_sum_list[n_class][n_sum]
+        if n_class == 1:
+            if n_max < n_sum:
+                n_given_sum_list[n_class][n_sum] = 0
+                return 0
+            else:
+                n_given_sum_list[n_class][n_sum] = 1/math.factorial(x_0 - n_sum-1)
+                return n_given_sum_list[n_class][n_sum]
+        if n_class < 1 and n_sum!=0:
+            return 0
+        n = 0
+        for i in range(min(n_max,n_sum)+1):
+            n += 1/math.factorial(x_0-i-1)*_n_given_sum_(n_class-1,n_sum-i,n_max)
+        n_given_sum_list[n_class][n_sum] = n
+        return n
+    return _n_given_sum_(n_class,n_sum,n_max)
+
 class DevSetTheory:
-    def __init__(self,d_m):
-        self.d = d_m
-        self.acc_list,self.p_alpha_list= self.p_alphas()
+    def __init__(self,d_matrix):
+        self.D_matrix = d_matrix
+        self.alpha_list, self.p_alpha_list= self.p_alphas()
+        self.d_alpha = self.alpha_list[1]-self.alpha_list[0]
 
     def log_likelihood(self,acc):
         log_L = 0
@@ -40,8 +64,8 @@ class DevSetTheory:
         prior = beta(1,1)
         P = 0
         n=100
-        for acc in np.linspace(0.01,0.99,n):
-            P+=prior.pdf(acc)*np.exp(self.log_likelihood(self.d,acc))*1/n
+        for acc in np.linspace(1e-6, 1-1e-6,n):
+            P+=prior.pdf(acc)*np.exp(self.log_likelihood(acc))*( 1 / n)
         return P
     def p_alphas(self):
         P_d_matrix = self.P_d()
@@ -49,18 +73,18 @@ class DevSetTheory:
         n = 100
         p_alpha_list = []
         acc_list = []
-        for acc in np.linspace(0.01, 0.99, n):
-            p = prior.pdf(acc) * np.exp(self.log_likelihood(acc)) / P_d_matrix * 1 / n
+        for acc in np.linspace(1e-6, 1-1e-6, n):
+            p = prior.pdf(acc) * np.exp(self.log_likelihood(acc)) / P_d_matrix
             p_alpha_list.append(p)
             acc_list.append(acc)
         return acc_list,p_alpha_list
 
     def p_acc_greater(self,target_acc):
         acc_greater = 0
-        for i in range(len(self.acc_list)):
-            acc = self.acc_list[i]
+        for i in range(len(self.alpha_list)):
+            acc = self.alpha_list[i]
             if acc > target_acc:
-                acc_greater+=self.p_alpha_list[i]
+                acc_greater+=self.p_alpha_list[i]*self.d_alpha
         return acc_greater
 
     def feasibility_test(self,epsilon = 0.7):
@@ -70,16 +94,51 @@ class DevSetTheory:
         :param epsilon: threshold of the estimated accuracy that make the task considered to be feasible
         :return: the probability of feasibility
         """
-        self.p_acc_greater(epsilon)
-        return self.acc_greater
+        acc_greater = self.p_acc_greater(epsilon)
+        return acc_greater
 
+
+    def p_one_dim(self,acc,i_dim):
+        n_class = self.D_matrix.shape[0]
+        dev_size = np.sum(self.D_matrix[i_dim, :])
+        x_0_min = int(math.ceil((dev_size + n_class - 1) / n_class))
+
+        def p(x_0):
+            y_sum = n_class * x_0 - dev_size - (n_class - 1)
+
+            return math.factorial(dev_size) / math.factorial(x_0) * math.exp(
+                math.log(acc) * x_0 + math.log((1 - acc) / (n_class - 1)) * (dev_size - x_0)) * \
+                   n_given_sum(n_class - 1, y_sum, x_0 - 1, x_0)
+
+        prob = 0
+        for x_0 in range(x_0_min, dev_size + 1):
+            prob += p(x_0)
+        return prob
+
+    def dev_set_sufficiency_test(self):
+        """
+        test whether the current dev set is sufficient.
+        This offers a guidance on whether you should use a larger dev set.
+        :return: the lower-bound probability whether the dev set is sufficiency enough
+         to obtain the correct class-cluster mapping
+        """
+        p = 0
+        for i in range(len(self.alpha_list)):
+            alpha = self.alpha_list[i]
+            p_alpha = self.p_alpha_list[i]
+            pl = 1
+            for i_dim in range(self.D_matrix.shape[0]):
+                pl*=self.p_one_dim(alpha, i_dim)
+            p = p + pl*p_alpha*self.d_alpha
+        return p
 
 
 if __name__ == "__main__":
-    d = generate_d_matrix(15,0.72,2)
+    d = generate_d_matrix(25,0.77,3)
     theory = DevSetTheory(d)
     acc_greater = theory.p_acc_greater(0.7)
     ps = theory.p_alpha_list
     print(acc_greater)
-    plt.plot(np.linspace(0.01, 0.99, 100),ps)
-    plt.show()
+    print(theory.dev_set_sufficiency_test())
+    #plt.plot(np.linspace(0.01, 0.99, 100),ps)
+    #plt.show()
