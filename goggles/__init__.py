2ae3795e722b1cb408ea6207d45920ea120c09a4
wurenzhi <wurenzhi@foxmail.com>
2019-08-10 22:22:44 -0400

    update

---
+++ goggles/__init__.py
@@ -0,0 +1,4 @@
+from goggles.inference_models.hierarchical_model import infer_labels,generate_labels
+from goggles.affinity_matrix_construction.construct import construct_affinity_matrices
+
+__version__ = '0.1'
--- goggles/inference_models/hierarchical_model.py
+++ goggles/inference_models/hierarchical_model.py
@@ -1,9 +1,10 @@
-from .semi_supervised_models import SemiGMM,SemiBMM
+from goggles.inference_models.semi_supervised_models import SemiGMM,SemiBMM
 import numpy as np
 from tqdm import tqdm
+from goggles.affinity_matrix_construction.construct import construct_affinity_matrices
 
 
-def generate_labels(affinity_matrix_list, dev_set_indices,dev_set_labels):
+def infer_labels(affinity_matrix_list, dev_set_indices, dev_set_labels):
     """
     Generate labels by a hierarchical inference model
     :param affinity_matrix_list:
@@ -14,11 +15,24 @@ def generate_labels(affinity_matrix_list, dev_set_indices,dev_set_labels):
     LPs = []
     for af_matrix in tqdm(affinity_matrix_list):
         base_model = SemiGMM(covariance_type="diag",n_components=n_classes)
-        base_model.fit(af_matrix,dev_set_indices,dev_set_labels)
-        lp = np.array(base_model.predict_proba(af_matrix))
+        lp = base_model.fit_predict(af_matrix,dev_set_indices,dev_set_labels)
         LPs.append(lp)
     LPs_array = np.hstack(LPs)
     ensemble_model = SemiBMM(n_components=n_classes)
     predicted_labels = ensemble_model.fit_predict(LPs_array,dev_set_indices,dev_set_labels)
     return predicted_labels
 
+
+def generate_labels(path_to_images,dev_set_indices, dev_set_labels):
+    """
+    :param path_to_images: path to the folder where images need to be labeled
+    :param dev_set_indices: list of indices of the images in the development set
+    :param dev_set_labels: list of labels of the images in the development set
+    :return:
+    """
+    affinity_matrix_list = construct_affinity_matrices(path_to_images)
+    predicted_labels = infer_labels(affinity_matrix_list, dev_set_indices, dev_set_labels)
+    return predicted_labels
+
+if __name__ == "__main__":
+    print(generate_labels("",[1,2,3,4,5],[0,0,1,1,1]))
