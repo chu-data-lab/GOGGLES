c4a0b03c34bd6f4e8a5997c248ea63da88aaf201
wurenzhi <wurenzhi@foxmail.com>
2019-08-12 09:28:12 -0400

    update

--- goggles/affinity_matrix_construction/construct.py
+++ goggles/affinity_matrix_construction/construct.py
@@ -9,10 +9,3 @@ def construct_affinity_matrices(path_to_image_foler=""):
     for i in range(10):
         matrix_list.append(np.random.rand(100,100))
     return matrix_list
-
-def obtain_dev_set():
-    """
-    :return: development set, a list a tuples [(index of the image, class label),...]
-    """
-    dev_set = [(25,0),(30,0),(51,1),(52,1)]
-    return dev_set
