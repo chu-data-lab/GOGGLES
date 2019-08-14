ed05fbd9fca3a30e1622ec2423acff78320ea4c2
wurenzhi <wurenzhi@foxmail.com>
2019-08-14 14:20:12 -0400

    delete generate_labels

--- goggles/inference_models/hierarchical_model.py
+++ goggles/inference_models/hierarchical_model.py
@@ -25,24 +25,3 @@ def infer_labels(affinity_matrix_list, dev_set_indices, dev_set_labels,seed=0,ev
     ensemble_model = SemiBMM(n_components=n_classes)
     predicted_labels = ensemble_model.fit_predict(LPs_array,dev_set_indices,dev_set_labels,evaluate)
     return predicted_labels
-
-
-def generate_labels(path_to_images,dev_set_indices, dev_set_labels, seed = 0,evaluate = True):
-    """
-    generate labels for images
-    :param path_to_images: path to the folder where images need to be labeled
-    :param dev_set_indices: list of indices of the images in the development set
-    :param dev_set_labels: list of labels of the images in the development set
-    :return: probability of each image (totaling N) belonging to each class (totaling K),
-         a N X K numpy array
-    """
-    random.seed(seed)
-    np.random.seed(seed)
-    affinity_matrix_list = construct_image_affinity_matrices(path_to_images)
-    predicted_labels = infer_labels(affinity_matrix_list, dev_set_indices, dev_set_labels,seed,evaluate)
-    return predicted_labels
-
-
-if __name__ == "__main__":
-
-    labels = generate_labels("",[1,2,3,4,5,6,7,8,9,10],[0,0,0,0,0,1,1,1,1,1],seed=0)
