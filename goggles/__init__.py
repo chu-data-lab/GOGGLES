76520b3fa84c4217e059cf05c52a461c4b7baaf2
wurenzhi <wurenzhi@foxmail.com>
2019-08-14 15:18:05 -0400

    add many __init__.py

--- goggles/__init__.py
+++ goggles/__init__.py
@@ -1,5 +1,5 @@
-from goggles.inference_models.hierarchical_model import infer_labels
-from goggles.utils.dataset import GogglesDataset
-from goggles.affinity_matrix_construction.construct import construct_image_affinity_matrices
+from .inference_models.hierarchical_model import infer_labels
+from .utils.dataset import GogglesDataset
+from .affinity_matrix_construction.construct import construct_image_affinity_matrices
 
 __version__ = '0.1'
