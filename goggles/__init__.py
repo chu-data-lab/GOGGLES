deeb03f0e0fcc0f808081da5aa4b71b359d552f9
wurenzhi <wurenzhi@foxmail.com>
2019-08-14 14:19:38 -0400

    update

--- goggles/__init__.py
+++ goggles/__init__.py
@@ -1,4 +1,5 @@
-from goggles.inference_models.hierarchical_model import infer_labels,generate_labels
+from goggles.inference_models.hierarchical_model import infer_labels
+from goggles.utils.dataset import GogglesDataset
 from goggles.affinity_matrix_construction.construct import construct_image_affinity_matrices
 
 __version__ = '0.1'
--- goggles/affinity_matrix_construction/construct.py
+++ goggles/affinity_matrix_construction/construct.py
@@ -1,12 +1,13 @@
 import numpy as np
+from goggles.affinity_matrix_construction.image_AF.neural_network_AFs import nn_AFs
 
 
-def construct_affinity_matrices(path_to_image_foler=""):
+def construct_image_affinity_matrices(dataset,cache=True):
     """
-    :param path_to_image_folder: path to the folder where images need to be labeled
+    :param GogglesDataset instance
     :return: a list of affinity matrices
     """
     matrix_list = []
-    for i in range(10):
-        matrix_list.append(np.random.rand(100,100))
+    for layer_idx in [4,9,16,23,30]:#all max pooling layers
+        matrix_list.extend(nn_AFs(dataset,layer_idx,10,cache))
     return matrix_list
