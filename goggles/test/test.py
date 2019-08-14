97e3590975e6c9df2d42aa61a9c4e4bb2ebad689
wurenzhi <wurenzhi@foxmail.com>
2019-08-14 16:19:44 -0400

    update

--- goggles/test/test.py
+++ goggles/test/test.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score
 if __name__ == '__main__':
-    dataset = GogglesDataset.load_all_data("data/cub_dataset/images")
+    dataset = GogglesDataset.load_all_data("../data/cub_dataset/images")
     afs = construct_image_affinity_matrices(dataset)
     dev_set_indices, dev_set_labels = [0,1,2,90,91,92],[0,0,0,1,1,1]
-    y_true = pd.read_csv("data/cub_dataset/labels.csv")
+    y_true = pd.read_csv("../data/cub_dataset/labels.csv")
     y_true = y_true['y'].values
     y_true[:int(y_true.shape[0]/2)] = 0
     prob = infer_labels(afs,dev_set_indices,dev_set_labels)
